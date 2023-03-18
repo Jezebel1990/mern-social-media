5d94b1def34b0981cbcede4e6566d2618b581a84
Jezebel1990 <jezebelguedes@gmail.com>
2023-03-18 00:46:46 -0300

    add logging in

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 /*CONFIGURATIONS */
@@ -31,13 +32,17 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
-    }
+    },
 });
 const upload = multer({ storage });
 
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+
+
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 /* MONGOOSE SETUP /*/
 const PORT = process.env.PORT || 6001;
