942843952a74457adb22a18a7b164e9b965d2180
Jezebel1990 <jezebelguedes@gmail.com>
2023-03-21 01:15:27 -0300

    update client

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -43,3 +43,4 @@ export const authSlice = createSlice({
     }
 })
 export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost} = authSlice.actions;
+export default authSlice.reducer;
